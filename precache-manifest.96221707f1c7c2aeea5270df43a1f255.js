c30e7612ea49b837e9b5f230e6cd6f761cfaa0fc
杨婧怡 <yangjingyi@yangjingyideMacBook-Pro.local>
2020-08-15 12:54:34 +0800

    Updates

---
+++ precache-manifest.96221707f1c7c2aeea5270df43a1f255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886588936e7d7b50bb531e0f1e57792e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d1f2088fee587dd2c41a",
+    "url": "/monsters-rolodex/static/css/main.ab8297fb.chunk.css"
+  },
+  {
+    "revision": "5e45cad78f733d48cb88",
+    "url": "/monsters-rolodex/static/js/2.0d085a21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0d085a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f2088fee587dd2c41a",
+    "url": "/monsters-rolodex/static/js/main.aff262c2.chunk.js"
+  },
+  {
+    "revision": "5e2ca95a33cf98ea72bc",
+    "url": "/monsters-rolodex/static/js/runtime-main.ffc220f2.js"
+  }
+]);
